f63d03f9c1f82c9bbef1aae89d6274f9e7323304
rahul-shiv <rahul.shivaprasad@gmail.com>
2018-06-04 13:04:35 +0530

    Add files via upload

---
+++ create.sh
@@ -0,0 +1,15 @@
+CNI_VERSION="v0.6.0"
+sudo mkdir -p /opt/cni/bin
+curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-amd64-${CNI_VERSION}.tgz" | tar -C . -xz
+sudo mv * /opt/cni/bin
+
+RELEASE="$(curl -sSL https://dl.k8s.io/release/stable.txt)"
+
+sudo mkdir -p /opt/bin
+curl -L --remote-name-all https://storage.googleapis.com/kubernetes-release/release/${RELEASE}/bin/linux/amd64/{kubeadm,kubelet,kubectl}
+chmod +x {kubeadm,kubelet,kubectl}
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/kubelet.service" | sed "s:/usr/bin:/opt/bin:g" > kubelet.service
+sudo mv kubelet.service /etc/systemd/system/
+sudo mkdir -p /etc/systemd/system/kubelet.service.d
+curl -sSL "https://raw.githubusercontent.com/kubernetes/kubernetes/${RELEASE}/build/debs/10-kubeadm.conf" | sed "s:/usr/bin:/opt/bin:g" > 10-kubeadm.conf
+sudo mv 10-kubeadm.conf /etc/systemd/system/kubelet.service.d/
