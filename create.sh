61721ed1283f88d224340f9dd77463d1206ccc8c
rahul-shiv <rahul.shivaprasad@gmail.com>
2018-06-04 13:50:32 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 CNI_VERSION="v0.6.0"
 sudo mkdir -p /opt/cni/bin
 curl -L "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-amd64-${CNI_VERSION}.tgz" | tar -C . -xz
